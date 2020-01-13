322297e9bae10b7dc68470e53213f9ce080ef2dc
Blake K <bk24637@gmail.com>
2020-01-13 17:14:29 -0500

    Uncommented test files.

--- test11.cpp
+++ test11.cpp
@@ -1,148 +1,148 @@
-////--------------------------------------------------------------------
-////
-////  Laboratory 11                                          test11.cpp
-////
-////  Test program for the operations in the Heap ADT
-////
-////--------------------------------------------------------------------
-//#include <iostream>
-//#include <string>
-//#include <cctype>
-//using namespace std;
-//#include "Heap.cpp"
-//#include "show11.cpp"
-//#include "config.h"
-////--------------------------------------------------------------------
-//// Prototypes
-//void printHelp();
-////--------------------------------------------------------------------
-////
-//// Declaration for the heap data item class
-////
-//template < typename KeyType >
-//class TestDataItem
-//{
-//public:
-//	TestDataItem()
-//	{
-//		priority = -1;
-//	}
-//	void setPriority(KeyType newPty)
-//	{
-//		priority = newPty;
-//	}            // Set the priority
-//	KeyType getPriority() const
-//	{
-//		return priority;
-//	}              // Returns the priority
-//private:
-//	KeyType priority;                    // Priority for the data item
-//};
+//--------------------------------------------------------------------
 //
-//template < typename KeyType = int >
-//class Greater {
-//public:
-//	bool operator()(const KeyType& a, const KeyType& b) const { return a > b; }
-//};
+//  Laboratory 11                                          test11.cpp
 //
-//int main()
-//{
-//	// Greater<> uses the default int type for its KeyType
-//	Heap<TestDataItem<int>, int, Greater<> > testHeap(8);  // Test heap
-//	TestDataItem<int> testDataItem;       // Heap data item
-//	int inputPty;                        // User input priority
-//	char cmd;                             // Input command
-//	printHelp();
-//	do
-//	{
-//		testHeap.showStructure();                     // Output heap
-//		cout << endl << "Command: ";                  // Read command
-//		cin >> cmd;
-//		cmd = toupper(cmd);                    // Upcase input
-//		if (cmd == '+')
-//			cin >> inputPty;
-//		switch (cmd)
-//		{
-//		case 'H':
-//			printHelp();
-//			break;
-//		case '+':                                  // insert
-//			testDataItem.setPriority(inputPty);
-//			try {
-//				cout << "Insert : priority = " << testDataItem.getPriority()
-//					<< endl;
-//				testHeap.insert(testDataItem);
-//			}
-//			catch (logic_error) {
-//				cout << "Error: Heap is full" << endl;
-//			}
-//			break;
-//		case '-':                                  // remove
-//			try {
-//				testDataItem = testHeap.remove();
-//				cout << "Removed data item : "
-//					<< " priority = " << testDataItem.getPriority() << endl;
-//			}
-//			catch (logic_error) {
-//				cout << "Error: Heap is empty" << endl;
-//			}
-//			break;
-//		case 'C':                                  // clear
-//			cout << "Clear the heap" << endl;
-//			testHeap.clear();
-//			break;
-//		case 'E':                                  // isEmpty
-//			if (testHeap.isEmpty())
-//				cout << "Heap is empty" << endl;
-//			else
-//				cout << "Heap is NOT empty" << endl;
-//			break;
-//		case 'F':                                  // isFull
-//			if (testHeap.isFull())
-//				cout << "Heap is full" << endl;
-//			else
-//				cout << "Heap is NOT full" << endl;
-//			break;
-//		case 'A':
-//		{
-//			Heap< TestDataItem<int>, int, Greater<> > testHeap2(testHeap);
-//			cout << "Other heap:" << endl;
-//			testHeap2.showStructure();
-//			cout << endl << "This heap:" << endl;
-//			break;
-//		}
-//#if LAB11_TEST1
-//		case 'W':                              // Programming Exercise 3
-//			cout << "Levels :" << endl;
-//			testHeap.writeLevels();
-//			break;
-//#endif      // LAB11_TEST1
-//		case 'Q':                              // Quit test program
-//			break;
-//		default:                               // Invalid command
-//			cout << "Inactive or invalid command" << endl;
-//		}
-//	} while (cmd != 'Q');
-//	return 0;
-//}
-////--------------------------------------------------------------------
-//void printHelp()
-//{
-//	cout << endl << "Commands C:" << endl;
-//	cout << "  H    : Help (displays this message)" << endl;
-//	cout << "  +pty : Insert data item with priority pty" << endl;
-//	cout << "  -    : Remove highest priority data item" << endl;
-//	cout << "  C    : Clear the heap" << endl;
-//	cout << "  E    : Empty heap?" << endl;
-//	cout << "  F    : Full heap?" << endl;
-//	cout << "  A    : Copy this heap into another" << endl;
-//	cout << "  W    : Write levels   ("
-//#if LAB11_TEST1
-//		"Active   "
-//#else
-//		"Inactive "
-//#endif      //LAB11_TEST1
-//		<< ": Programming Exercise 3)" << endl;
-//	cout << "  Q    : Quit the test program" << endl;
-//	cout << endl;
-//}
+//  Test program for the operations in the Heap ADT
+//
+//--------------------------------------------------------------------
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+#include "Heap.cpp"
+#include "show11.cpp"
+#include "config.h"
+//--------------------------------------------------------------------
+// Prototypes
+void printHelp();
+//--------------------------------------------------------------------
+//
+// Declaration for the heap data item class
+//
+template < typename KeyType >
+class TestDataItem
+{
+public:
+	TestDataItem()
+	{
+		priority = -1;
+	}
+	void setPriority(KeyType newPty)
+	{
+		priority = newPty;
+	}            // Set the priority
+	KeyType getPriority() const
+	{
+		return priority;
+	}              // Returns the priority
+private:
+	KeyType priority;                    // Priority for the data item
+};
+
+template < typename KeyType = int >
+class Greater {
+public:
+	bool operator()(const KeyType& a, const KeyType& b) const { return a > b; }
+};
+
+int main()
+{
+	// Greater<> uses the default int type for its KeyType
+	Heap<TestDataItem<int>, int, Greater<> > testHeap(8);  // Test heap
+	TestDataItem<int> testDataItem;       // Heap data item
+	int inputPty;                        // User input priority
+	char cmd;                             // Input command
+	printHelp();
+	do
+	{
+		testHeap.showStructure();                     // Output heap
+		cout << endl << "Command: ";                  // Read command
+		cin >> cmd;
+		cmd = toupper(cmd);                    // Upcase input
+		if (cmd == '+')
+			cin >> inputPty;
+		switch (cmd)
+		{
+		case 'H':
+			printHelp();
+			break;
+		case '+':                                  // insert
+			testDataItem.setPriority(inputPty);
+			try {
+				cout << "Insert : priority = " << testDataItem.getPriority()
+					<< endl;
+				testHeap.insert(testDataItem);
+			}
+			catch (logic_error) {
+				cout << "Error: Heap is full" << endl;
+			}
+			break;
+		case '-':                                  // remove
+			try {
+				testDataItem = testHeap.remove();
+				cout << "Removed data item : "
+					<< " priority = " << testDataItem.getPriority() << endl;
+			}
+			catch (logic_error) {
+				cout << "Error: Heap is empty" << endl;
+			}
+			break;
+		case 'C':                                  // clear
+			cout << "Clear the heap" << endl;
+			testHeap.clear();
+			break;
+		case 'E':                                  // isEmpty
+			if (testHeap.isEmpty())
+				cout << "Heap is empty" << endl;
+			else
+				cout << "Heap is NOT empty" << endl;
+			break;
+		case 'F':                                  // isFull
+			if (testHeap.isFull())
+				cout << "Heap is full" << endl;
+			else
+				cout << "Heap is NOT full" << endl;
+			break;
+		case 'A':
+		{
+			Heap< TestDataItem<int>, int, Greater<> > testHeap2(testHeap);
+			cout << "Other heap:" << endl;
+			testHeap2.showStructure();
+			cout << endl << "This heap:" << endl;
+			break;
+		}
+#if LAB11_TEST1
+		case 'W':                              // Programming Exercise 3
+			cout << "Levels :" << endl;
+			testHeap.writeLevels();
+			break;
+#endif      // LAB11_TEST1
+		case 'Q':                              // Quit test program
+			break;
+		default:                               // Invalid command
+			cout << "Inactive or invalid command" << endl;
+		}
+	} while (cmd != 'Q');
+	return 0;
+}
+//--------------------------------------------------------------------
+void printHelp()
+{
+	cout << endl << "Commands C:" << endl;
+	cout << "  H    : Help (displays this message)" << endl;
+	cout << "  +pty : Insert data item with priority pty" << endl;
+	cout << "  -    : Remove highest priority data item" << endl;
+	cout << "  C    : Clear the heap" << endl;
+	cout << "  E    : Empty heap?" << endl;
+	cout << "  F    : Full heap?" << endl;
+	cout << "  A    : Copy this heap into another" << endl;
+	cout << "  W    : Write levels   ("
+#if LAB11_TEST1
+		"Active   "
+#else
+		"Inactive "
+#endif      //LAB11_TEST1
+		<< ": Programming Exercise 3)" << endl;
+	cout << "  Q    : Quit the test program" << endl;
+	cout << endl;
+}
--- test11pq.cpp
+++ test11pq.cpp
@@ -1,108 +1,108 @@
-////--------------------------------------------------------------------
-////
-////  Laboratory 11, Programming Exercise 1                test11pq.cpp
-////
-////  Test program for the operations in the Priority Queue ADT
-////
-////--------------------------------------------------------------------
-//#include <iostream>
-//#include <cctype>
-//#include "PriorityQueue.cpp"
-//#include "show11.cpp"
-//using namespace std;
-//void printHelp();
-////--------------------------------------------------------------------
-////
-//// Declaration for the priority queue data item class
-////
-//class TestData
-//{
-//public:
-//	void setPriority(int newPriority)
-//	{
-//		priority = newPriority;
-//	}  // Set the priority
-//	int getPriority() const
-//	{
-//		return priority;
-//	}         // Returns the priority
-//private:
-//	int priority;                    // Priority for the data item
-//};
-////--------------------------------------------------------------------
-//int main()
-//{
-//	PriorityQueue<TestData> testQueue(8);   // Test priority queue
-//	TestData testData;                 // Queue data item
-//	int inputPty;                      // User input priority
-//	char cmd;                          // Input command
-//	printHelp();
-//	do
-//	{
-//		testQueue.showStructure();                    // Output queue
-//		cout << endl << "Command (H for help): ";     // Read command
-//		cin >> cmd;
-//		cmd = toupper(cmd);                    // Normalize to uppercase
-//		if ((cmd == '+') || (cmd == '>'))
-//			cin >> inputPty;
-//		switch (cmd)
-//		{
-//		case '+':                                  // enqueue
-//			try {
-//				testData.setPriority(inputPty);
-//				cout << "Enqueue :  pty = " << testData.getPriority()
-//					<< endl;
-//				testQueue.enqueue(testData);
-//			}
-//			catch (logic_error) {
-//				cout << "Error: Queue is full" << endl;
-//			}
-//			break;
-//		case '-':                                  // dequeue
-//			try {
-//				testData = testQueue.dequeue();
-//				cout << "Dequeued :  pty = " << testData.getPriority()
-//					<< endl;
-//			}
-//			catch (logic_error) {
-//				cout << "Error: Queue is empty" << endl;
-//			}
-//			break;
-//		case 'C':
-//			cout << "Clear the queue" << endl;
-//			testQueue.clear();
-//			break;
-//		case 'E':                                  // isEmpty
-//			if (testQueue.isEmpty())
-//				cout << "Queue is empty" << endl;
-//			else
-//				cout << "Queue is NOT empty" << endl;
-//			break;
-//		case 'F':                                  // isFull
-//			if (testQueue.isFull())
-//				cout << "Queue is full" << endl;
-//			else
-//				cout << "Queue is NOT full" << endl;
-//			break;
-//		case 'H':                                  // Print out help menu
-//			printHelp();
-//			break;
-//		case 'Q':                                  // Quit test program
-//			break;
-//		default:                                   // Invalid command
-//			cout << "Invalid command" << endl;
-//		}
-//	} while (cmd != 'Q');
-//	return 0;
-//}
-//void printHelp() {
-//	cout << endl << "Commands:" << endl;
-//	cout << "  +x : Enqueue data item with priority x" << endl;
-//	cout << "  -  : Dequeue data item" << endl;
-//	cout << "  C  : Clear the queue" << endl;
-//	cout << "  E  : Empty queue?" << endl;
-//	cout << "  F  : Full queue?" << endl;
-//	cout << "  H  : Print this help message" << endl;
-//	cout << "  Q  : Quit the test program" << endl;
-//	cout << endl;
-//}
+//--------------------------------------------------------------------
+//
+//  Laboratory 11, Programming Exercise 1                test11pq.cpp
+//
+//  Test program for the operations in the Priority Queue ADT
+//
+//--------------------------------------------------------------------
+#include <iostream>
+#include <cctype>
+#include "PriorityQueue.cpp"
+#include "show11.cpp"
+using namespace std;
+void printHelp();
+//--------------------------------------------------------------------
+//
+// Declaration for the priority queue data item class
+//
+class TestData
+{
+public:
+	void setPriority(int newPriority)
+	{
+		priority = newPriority;
+	}  // Set the priority
+	int getPriority() const
+	{
+		return priority;
+	}         // Returns the priority
+private:
+	int priority;                    // Priority for the data item
+};
+//--------------------------------------------------------------------
+int main()
+{
+	PriorityQueue<TestData> testQueue(8);   // Test priority queue
+	TestData testData;                 // Queue data item
+	int inputPty;                      // User input priority
+	char cmd;                          // Input command
+	printHelp();
+	do
+	{
+		testQueue.showStructure();                    // Output queue
+		cout << endl << "Command (H for help): ";     // Read command
+		cin >> cmd;
+		cmd = toupper(cmd);                    // Normalize to uppercase
+		if ((cmd == '+') || (cmd == '>'))
+			cin >> inputPty;
+		switch (cmd)
+		{
+		case '+':                                  // enqueue
+			try {
+				testData.setPriority(inputPty);
+				cout << "Enqueue :  pty = " << testData.getPriority()
+					<< endl;
+				testQueue.enqueue(testData);
+			}
+			catch (logic_error) {
+				cout << "Error: Queue is full" << endl;
+			}
+			break;
+		case '-':                                  // dequeue
+			try {
+				testData = testQueue.dequeue();
+				cout << "Dequeued :  pty = " << testData.getPriority()
+					<< endl;
+			}
+			catch (logic_error) {
+				cout << "Error: Queue is empty" << endl;
+			}
+			break;
+		case 'C':
+			cout << "Clear the queue" << endl;
+			testQueue.clear();
+			break;
+		case 'E':                                  // isEmpty
+			if (testQueue.isEmpty())
+				cout << "Queue is empty" << endl;
+			else
+				cout << "Queue is NOT empty" << endl;
+			break;
+		case 'F':                                  // isFull
+			if (testQueue.isFull())
+				cout << "Queue is full" << endl;
+			else
+				cout << "Queue is NOT full" << endl;
+			break;
+		case 'H':                                  // Print out help menu
+			printHelp();
+			break;
+		case 'Q':                                  // Quit test program
+			break;
+		default:                                   // Invalid command
+			cout << "Invalid command" << endl;
+		}
+	} while (cmd != 'Q');
+	return 0;
+}
+void printHelp() {
+	cout << endl << "Commands:" << endl;
+	cout << "  +x : Enqueue data item with priority x" << endl;
+	cout << "  -  : Dequeue data item" << endl;
+	cout << "  C  : Clear the queue" << endl;
+	cout << "  E  : Empty queue?" << endl;
+	cout << "  F  : Full queue?" << endl;
+	cout << "  H  : Print this help message" << endl;
+	cout << "  Q  : Quit the test program" << endl;
+	cout << endl;
+}
